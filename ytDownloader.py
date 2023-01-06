8da859db9c1a06d38a04c074357055f5cf177cf8
benjamindavisdc <112713133+benjamindavisdc@users.noreply.github.com>
2023-01-05 21:54:15 -0600

    Add files via upload
    Uploaded a simple Python script which can be used to download Youtube videos at various resolutions or audio only.

---
+++ ytDownloader.py
@@ -0,0 +1,15 @@
+from pytube import YouTube
+from sys import argv
+
+link = argv[1]
+yt = YouTube(link)
+
+print("Title: ", yt.title)
+
+print("Views: ", yt.views)
+
+
+yd = yt.streams.get_audio_only()
+yd = yt.streams.get_by_resolution('360p')
+
+yd.download('/Users/#Username')
